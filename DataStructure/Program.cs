cf9bba4924cd12482d70247bdf5628917ca84af3
Vishal Singh <VishalSingh1597>
2021-05-08 01:16:57 +0530

    Added Insertion Between 56 and 70

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -4,18 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LinkedList
+namespace DataStructure
 {
     class Program
     {
         static void Main(string[] args)
         {
-            LinkedList list = new LinkedList();
-            list.InsertLast(56);
-            list.InsertLast(30);
-            list.InsertLast(70);
+            Console.WriteLine("-------Welcome To Data Structure Program-------");
+
+            LinkedList list = new LinkedList();//creating object of linkedlist class
+            //list.InsertLast(56);
+            //list.InsertLast(30);
+            //list.InsertLast(70);
+
+            //list.InsertFront(70);
+            //list.InsertFront(30);
+            //list.InsertFront(56);
+
+            list.InsertFront(70);
+            list.InsertFront(56);
+
+            list.InsertAtParticularPosition(2, 30);
             list.Display();
-            Console.ReadLine();
+
+            Console.Read();
         }
     }
 }
