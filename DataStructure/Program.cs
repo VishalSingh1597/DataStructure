7b644dceb216028490d00f9b03efad000274d5b5
Vishal Singh <VishalSingh1597>
2021-05-10 00:25:13 +0530

    Added Insert After

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -8,26 +8,31 @@ namespace DataStructure
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("-------Welcome To Data Structure Program-------");
-
-            LinkedList list = new LinkedList();//creating object of linkedlist class
+            Console.WriteLine("Welcome to data structure");
+            LinkedList list = new LinkedList();
             //list.InsertLast(56);
             //list.InsertLast(30);
             //list.InsertLast(70);
 
-            //list.InsertFront(70);
-            //list.InsertFront(30);
-            //list.InsertFront(56);
-
             list.InsertFront(70);
+            list.InsertFront(30);
             list.InsertFront(56);
 
-            list.InsertAtParticularPosition(2, 30);
-            list.Display();
 
-            Console.Read();
+            //list.InsertFront(70);
+            //list.InsertFront(56);
+            //list.InsertAtParticularPosition(2, 30);
+
+            //list.DeleteFirstNode();
+
+            // list.DeleteLastNode();
+
+            list.Search(30);
+            list.Display();
+            Console.ReadLine();
+
         }
     }
 }
